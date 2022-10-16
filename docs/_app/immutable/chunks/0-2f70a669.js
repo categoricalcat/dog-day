480067720481132510ebbbc1b96bcbde5f2b9f20
walze <wallacefares@gmail.com>
2022-10-16 18:53:19 -0300

    feat: docs

---
+++ docs/_app/immutable/chunks/0-2f70a669.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-4dc315fd.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	compilerOptions: {},
 	kit: {
 		adapter: adapter({
 			precompress: true
